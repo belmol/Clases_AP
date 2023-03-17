d215c340aae4ac035d79e09397e5a002b35d1195
ncarlos725 <ncarlos725@gmail.com>
2023-03-17 14:52:35 -0300

    primer commit clase 1

---
+++ variables.js
@@ -0,0 +1,19 @@
+//ejercicio1/
+var auto = "corolla";
+console.log(auto);
+console.log(typeof(auto));
+//ejercicio2/
+auto=308; 
+console.log(auto);
+console.log(typeof(auto));
+//ejercicio3/
+auto="C4";
+console.log(auto);
+console.log(typeof(auto));
+//ejercicio4/
+var nombre="maria belen", apellido="molina";
+console.log(nombre+" "+apellido);
+//ejercicio5//
+var fruta= "naranja";
+var tipodelavariablefruta=typeof(fruta); 
+console.log(tipodelavariablefruta);
